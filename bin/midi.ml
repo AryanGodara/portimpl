abaf03f709c833a7aabecdaa898747244d90312f
Aryan Godara <aryangodara03@gmail.com>
2023-06-10 23:57:03 +0530

    first commit

---
+++ bin/midi.ml
@@ -0,0 +1,23 @@
+module Event = Portmidi.Portmidi_event
+
+(** Portmidi)error has function to convert it to type sexp; then use sexp library to convert it to string *)
+let error_to_string msg = 
+  Portmidi.Portmidi_error.sexp_of_t msg |> Sexplib0.Sexp.to_string
+
+let () = 
+match Portmidi.initialize () with
+  | Ok () -> ()
+  | Error _ -> failwith "error initializing portmidi"
+
+
+
+(** Define the message_on and message_off functions that need to be called to with a 5s delay in between *)
+let message_on ~note ~timestamp ~volume ~channel () =
+  let channel = 15 land channel in
+  let status = char_of_int (144 lor channel) in
+  Event.create ~status ~data1:note ~data2: volume ~timestamp
+
+let message_on ~note ~timestamp ~volume ~channel () =
+  let channel = 15 land channel in
+  let status = char_of_int (128 lor channel) in
+  Event.create ~status ~data1:note ~data2: volume ~timestamp
---
+++ bin/midi.mli
@@ -0,0 +1,15 @@
+module Event = Portmidi.Portmidi_event
+
+val error_to_string : Portmidi.Portmidi_error.t -> string
+
+val message_on : 
+  note:char -> timestamp:int32 -> volume:char -> channel:int -> unit -> Event.t
+val message_off : 
+  note:char -> timestamp:int32 -> volume:char -> channel:int -> unit -> Event.t
+
+module Device : sig
+  type t
+
+  val create : int -> t
+  val shutdown : t -> (unit, Portmidi.Portmidi_error.t) result
+end
